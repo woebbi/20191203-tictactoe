81d2c150954ecbe90a59c35b63738963d9e4a0b6
sirRippchen <woebbi@googlemail.com>
2019-12-03 13:15:09 +0100

    Initial commit

---
+++ app/src/main/java/com/woebbi/a20191203_tictactoe/MainActivity.java
@@ -0,0 +1,14 @@
+package com.woebbi.a20191203_tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
