1c9fd2b36d48812b6a71147270b6da26fed38559
sirRippchen <woebbi@googlemail.com>
2019-12-03 13:21:30 +0100

    #init

---
+++ app/src/main/java/com/woebbi/a20191203_tictactoe/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.woebbi.a20191203_tictactoe;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
